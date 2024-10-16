ebe637c3fc61157b0e3cb8f66f9a84ab44a0b955
Pavan Singh <psinghp888@gmail.com>
2024-10-16 15:51:28 +0530

    Routes and error handling

--- src/app.js
+++ src/app.js
@@ -2,13 +2,27 @@ const express= require("express");
 
 const app=express();
 
-app.use("/test",(req,res)=>{  
-    res.send("Hello from test!");
+app.use("/test",(req,res,next)=>{  
+    console.log("Hello from use test!");
+    next();
 });
 
 app.use("/home",(req,res)=>{
-    res.send("Hello from home!");
+    throw new Error("Error example working.")
+    // res.send("Hello from home!");
 });
 
+app.get("/test", 
+    (req,res,next)=>{ console.log("Hello from 1st get test!"); next()},
+    (req,res,next)=>{ console.log("Hello from 2nd get test!"); next()},
+    (req,res,next)=>{ res.send("Hello from 3rd get test!");}
+)
+
+app.use("/",(err,req,res,next)=>{
+    if(err){
+        console.log(err);
+        res.send("Something went wrong!!!")
+    }
+})
 
 app.listen(7777, () => console.log("Server is successfully listening on port 7777..."))
